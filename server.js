b867173c23a6bddb1766076db58cd374be2c8942
Avery Bloom <avery@wearestack.com>
2017-09-16 00:49:19 -0700

    trying something

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ I will respond to the following messages:
 
 // response to the user typing "help"
 slapp.message('help', ['mention', 'direct_message'], (msg) => {
-  msg.say("No!");
+  msg.say("No!"); 
 
 })
 
